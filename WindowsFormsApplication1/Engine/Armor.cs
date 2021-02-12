52aa10cbae9b294ec78152c2dd0055340fb59d6e
Daniel Wernz <dwernz@att.net>
2021-02-12 01:14:40 -0500

    Created Armor class
    Created armor class

---
+++ WindowsFormsApplication1/Engine/Armor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Armor : Item
+    {
+        public int MinimumDefense { set; get; }
+        public int MaximumDefense { set; get; }
+        public string Type { set; get; }
+
+        public Armor(int id, string name, string namePlural,
+            int minimumDefense, int maximumDefense, 
+            string type, int price) :
+            base(id, name, namePlural, price)
+        {
+            MinimumDefense = minimumDefense;
+            MaximumDefense = maximumDefense;
+            Type = type;
+        }
+    }
+}
